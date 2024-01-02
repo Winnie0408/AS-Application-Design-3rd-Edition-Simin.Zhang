bfadb97996609203b505b54036b376d49510b6e4
Winnie0408 <1196903869@qq.com>
2024-01-02 22:56:47 +0800

    存档-实验8习题2-设计一个简易记事本，以文件形式保存

--- app/src/main/java/com/hwinzniej/exp/MainActivity.java
+++ app/src/main/java/com/hwinzniej/exp/MainActivity.java
@@ -1,60 +1,54 @@
 package com.hwinzniej.exp;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.alibaba.fastjson2.function.impl.ToShort;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Button readBtn, saveBtn;
+    Button readBtn, saveBtn, exitBtn;
     EditText editText;
-    String fileName = "test.txt";
+    String fileName = "notebook.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editTextText);
+
+        editText = findViewById(R.id.editTextTextMultiLine);
         readBtn = findViewById(R.id.readButton);
         saveBtn = findViewById(R.id.saveButton);
+        exitBtn = findViewById(R.id.exitButton);
+
         readBtn.setOnClickListener(v -> {
-            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                File path = Environment.getExternalStorageDirectory();
-                File sdFile = new File(path, "Download/" + fileName);
-                try {
-                    FileInputStream fileInputStream = new FileInputStream(sdFile);
-                    byte[] bytes = new byte[1024];
-                    int len = fileInputStream.read(bytes);
-                    String text = new String(bytes, 0, len);
-                    editText.setText(text);
-                } catch (IOException e) {
-                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
+            try {
+                FileInputStream fileInputStream = openFileInput(fileName);
+                byte[] bytes = new byte[1024];
+                int len = fileInputStream.read(bytes);
+                String text = new String(bytes, 0, len);
+                editText.setText(text);
+                Toast.makeText(this, "读取成功", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(this, "出错了：" + e, Toast.LENGTH_SHORT).show();
             }
         });
         saveBtn.setOnClickListener(v -> {
             String text = editText.getText().toString();
-            String type = Environment.DIRECTORY_DOWNLOADS;
-            File fileDir = Environment.getExternalStoragePublicDirectory(type);
-            File sdFile = new File(fileDir + "/" + fileName);
             try {
-                FileOutputStream fileOutputStream = new FileOutputStream(sdFile);
+                FileOutputStream fileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
                 fileOutputStream.write(text.getBytes());
                 fileOutputStream.close();
-                Toast.makeText(this, "文件保存到" + sdFile, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "已保存", Toast.LENGTH_SHORT).show();
             } catch (IOException e) {
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "出错了：" + e, Toast.LENGTH_SHORT).show();
             }
-
         });
+        exitBtn.setOnClickListener(v -> finish());
     }
 }
